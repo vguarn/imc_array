47ad3f8f0509a7b9292ef43461ff899b796a6e16
Usuario <Usuario@CPD02-M13>
2024-05-07 10:38:47 -0300

    Alterações no projeto

--- imc_array/Program.cs
+++ imc_array/Program.cs
@@ -10,14 +10,14 @@ namespace imc_array
     {
         static void Main(string[] args)
         {
-            string[] nome = new string[5];
-            double[] peso = new double[5];
-            double[] altura = new double[5];
-            double[] imc = new double[5];
-            string[] imc2 = new string[5];
-            int[] acima = new int[5];
+            string[] nome = new string[4];
+            double[] peso = new double[4];
+            double[] altura = new double[4];
+            double[] imc = new double[4];
+            string[] imc2 = new string[4];
+            int acima = 0;
 
-            for (int i = 0; i <= 5; i++)
+            for (int i = 0; i <= 4; i++)
             {
                 Console.WriteLine("Digite o nome: ");
                 nome[i] = Console.ReadLine();
@@ -38,21 +38,35 @@ namespace imc_array
                 if (imc[i] >= 25.0&& imc[i] <= 29.9)
                 {
                     imc2[i] = "Levemente acima do peso!";
+                    acima++;
                 }
                 if (imc[i] >= 30.0 && imc[i] <= 34.9)
                 {
                     imc2[i] = "Obesidade grau I!";
+                    acima++;
+                    
                 }
                 if (imc[i] >= 35.0 && imc[i] <= 39.9)
                 {
                     imc2[i] = "Obesidade grau II!";
+                    acima++;
                 }
                 if (imc[i] > 40.0)
                 {
                     imc2[i] = "Obesidade grau III!";
+                    acima++;
                 }
             }
 
+            for (int j =0; j<=4; j++)
+            { 
+                Console.WriteLine("Nome: " + nome[j]);
+                Console.WriteLine("Peso: " + peso[j]);
+                Console.WriteLine("IMC: " + imc2[j]);
+            }
+             int acimapeso = acima / 100;
+
+            Console.WriteLine(acima + " pessoas estão acima do peso.");
            
 
             Console.ReadKey();
